22b09fef21a91e308d34a06dca416d2b521c6d0c
Nicolás Cámera <ncamera@genexus.com>
2022-12-16 22:08:00 -0300

    Add common.scss file to bundle common css
    This file will be preloaded in Angular applications.

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,7 @@ import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'reporting',
+  globalStyle: 'src/global/common.scss',
   outputTargets: [
     {
       type: 'dist',
