9a3ac022707c842f1134f3411271617dac88a36c
Nicolás Cámera <ncamera@genexus.com>
2023-05-17 00:29:14 -0300

    Add basic-types file

---
+++ src/common/basic-types.ts
@@ -0,0 +1,50 @@
+export type GeneratorType = "net" | "java";
+
+export type ServiceType = "metadata" | "data";
+
+export enum QueryViewerOutputType {
+  Card = "Card",
+  Chart = "Chart",
+  PivotTable = "PivotTable",
+  Table = "Table",
+  Map = "Map",
+  Default = "Default"
+}
+
+export enum QueryViewerChartType {
+  Column = "Column",
+  Column3D = "Column3D",
+  StackedColumn = "StackedColumn",
+  StackedColumn3D = "StackedColumn3D",
+  StackedColumn100 = "StackedColumn100",
+  Bar = "Bar",
+  StackedBar = "StackedBar",
+  StackedBar100 = "StackedBar100",
+  Area = "Area",
+  StackedArea = "StackedArea",
+  StackedArea100 = "StackedArea100",
+  SmoothArea = "SmoothArea",
+  StepArea = "StepArea",
+  Line = "Line",
+  StackedLine = "StackedLine",
+  StackedLine100 = "StackedLine100",
+  SmoothLine = "SmoothLine",
+  StepLine = "StepLine",
+  Pie = "Pie",
+  Pie3D = "Pie3D",
+  Doughnut = "Doughnut",
+  Doughnut3D = "Doughnut3D",
+  LinearGauge = "LinearGauge",
+  CircularGauge = "CircularGauge",
+  Radar = "Radar",
+  FilledRadar = "FilledRadar",
+  PolarArea = "PolarArea",
+  Funnel = "Funnel",
+  Pyramid = "Pyramid",
+  ColumnLine = "ColumnLine",
+  Column3DLine = "Column3DLine",
+  Timeline = "Timeline",
+  SmoothTimeline = "SmoothTimeline",
+  StepTimeline = "StepTimeline",
+  Sparkline = "Sparkline"
+}
