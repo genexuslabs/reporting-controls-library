d1af40ee133d580f9fe96736c3c7ac2e9a97c411
Nicolás Cámera <ncamera@genexus.com>
2023-05-17 00:30:22 -0300

    Add types for the JSONs

---
+++ src/services/types/constants.ts
@@ -0,0 +1,28 @@
+export const QueryViewerAxisOrderType = {
+  None: "None",
+  Ascending: "Ascending",
+  Descending: "Descending",
+  Custom: "Custom"
+};
+
+export const QueryViewerConditionOperator = {
+  Equal: "EQ",
+  LessThan: "LT",
+  GreaterThan: "GT",
+  LessOrEqual: "LE",
+  GreaterOrEqual: "GE",
+  NotEqual: "NE",
+  Interval: "IN"
+};
+
+export const QueryViewerExpandCollapseType = {
+  ExpandAllValues: "ExpandAllValues",
+  CollapseAllValues: "CollapseAllValues",
+  ExpandSomeValues: "ExpandSomeValues"
+};
+
+export const QueryViewerFilterType = {
+  ShowAllValues: "ShowAllValues",
+  HideAllValues: "HideAllValues",
+  ShowSomeValues: "ShowSomeValues"
+};
