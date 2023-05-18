32402c66f8f74763f5cb60c28fe24f7465c7378b
Nicolás Cámera <81186472+ncamera@users.noreply.github.com>
2023-05-17 22:36:36 -0300

    Add `gx-query-viewer-controller` to implement service consumption (#5)
    * Add basic-types file
    
    * Add types for the JSONs
    
    * Add utilities to manage service consumption
    
    * Add utilities to implement post parameters
    
    * Add metaData, data and attributeValues post-info for services consumption
    
    * Add service manager to implement requests
    
    * Add gx-query-viewer-controller to manage service consumption declaratively
    
    * Move constants to enums
    
    * Improve JSONs types
    
    * Add types for service responses
    
    This types will help us define the communication interface between the query-viewer and the query-viewer-controller
    
    * Add utilities for XML parser
    
    * Add XML parser for data service response
    
    * Add XML parser for metaData service response
    
    * Update controller to nest metaData and data requests

---
+++ src/services/services-manager.ts
@@ -0,0 +1,51 @@
+import { QueryViewer } from "./types/json";
+import { data, metaData } from "./post-info";
+import { parseObjectToFormData } from "./utils/common";
+import { GeneratorType, ServiceType } from "../common/basic-types";
+
+const STATE_DONE = 4;
+const STATUS_OK = 200;
+
+const GENERATOR: { [key in GeneratorType]: string } = {
+  net: "agxpl_get.aspx?",
+  java: "qviewer.services.agxpl_get?"
+};
+
+const SERVICE_NAME_MAP: { [key in ServiceType]: string } = {
+  metadata: "metadata",
+  data: "data"
+};
+
+const SERVICE_POST_INFO_MAP: {
+  [key in ServiceType]: (qViewer: QueryViewer) => any;
+} = {
+  metadata: metaData,
+  data: data
+};
+
+export const asyncServerCall = (
+  qViewer: QueryViewer,
+  baseUrl: string,
+  environment: GeneratorType,
+  serviceType: ServiceType,
+  callbackWhenReady: (xml: string) => void
+) => {
+  const serviceURL =
+    baseUrl + GENERATOR[environment] + SERVICE_NAME_MAP[serviceType];
+  const postInfo = parseObjectToFormData(
+    SERVICE_POST_INFO_MAP[serviceType](qViewer)
+  );
+
+  const xmlHttp = new XMLHttpRequest();
+
+  // Callback function when ready
+  xmlHttp.onload = () => {
+    if (xmlHttp.readyState === STATE_DONE && xmlHttp.status === STATUS_OK) {
+      callbackWhenReady(xmlHttp.responseText);
+    }
+  };
+
+  xmlHttp.open("POST", serviceURL); // async
+  xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xmlHttp.send(postInfo);
+};
