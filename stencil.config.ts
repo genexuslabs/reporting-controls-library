f457fbc7fd58d38ccb650b438702ccd9006d2670
Christian Milocanovich <c.milocanovich@globant.com>
2023-12-20 14:34:13 -0300

    update react output configuration

--- stencil.config.ts
+++ stencil.config.ts
@@ -91,7 +91,8 @@ export const config: Config = {
         "gx-table",
         "gx-textblock",
         "gx-video"
-      ]
+      ],
+      includeDefineCustomElements: true
     })
   ],
   bundles: [
