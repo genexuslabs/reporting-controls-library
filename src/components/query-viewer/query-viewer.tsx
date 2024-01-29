126384238da50f07ae377098fccb1bab43a500d5
Karen Baz <93329543+KBazGenexus@users.noreply.github.com>
2024-01-29 11:25:21 -0300

    Added the use of async and await to manage promises generated by the methods.
    Co-authored-by: Nicolás Cámera <81186472+ncamera@users.noreply.github.com>

--- src/components/query-viewer/query-viewer.tsx
+++ src/components/query-viewer/query-viewer.tsx
@@ -637,24 +637,24 @@ export class QueryViewer {
   }
 
   @Listen("TableOnPageChangeEvent")
-  handleTableOnDragundDropEvent(event: CustomEvent<TablePageChange>) {
+  async handleTableOnDragundDropEvent(event: CustomEvent<TablePageChange>) {
     let eventData;
 
     switch ((event as any).parameter.Navigation) {
       case PivotTableNavigation.OnFirstPage:
-        eventData = this.pivotRenderRef.firstPage();
+        eventData = await this.pivotRenderRef.firstPage();
         this.firstPage.emit(eventData);
         break;
       case PivotTableNavigation.OnLastPage:
-        eventData = this.pivotRenderRef.lastPage();
+        eventData = await this.pivotRenderRef.lastPage();
         this.lastPage.emit(eventData);
         break;
       case PivotTableNavigation.OnNextPage:
-        eventData = this.pivotRenderRef.nextPage();
+        eventData = await this.pivotRenderRef.nextPage();
         this.nextPage.emit(eventData);
         break;
       case PivotTableNavigation.OnPreviousPage:
-        eventData = this.pivotRenderRef.previousPage();
+        eventData = await this.pivotRenderRef.previousPage();
         this.previousPage.emit(eventData);
         break;
     }
